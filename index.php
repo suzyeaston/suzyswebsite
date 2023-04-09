2124e1a3318073c7e6475c97114f00ee0182bc1f
Suzy Easton <suzyeaston@icloud.com>
2023-04-08 18:14:46 -0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<main>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_excerpt(); ?>
+        </article>
+    <?php endwhile; endif; ?>
+</main>
+
+<?php get_footer(); ?>
