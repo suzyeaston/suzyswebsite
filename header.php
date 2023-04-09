92fded923f42ae66f90b561f3a47c5d52d7f9ca5
Suzy Easton <suzyeaston@icloud.com>
2023-04-08 18:25:42 -0700

    adding some stars

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<div id="starry-background"></div>
 <header>
     <nav>
         <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
