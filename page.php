4dbfac989f2c1bd6f8e07a0a9704feca7ea4c34c
Suzy Easton <suzyeaston@icloud.com>
2023-04-08 19:22:26 -0700

    Update page.php

--- page.php
+++ page.php
@@ -3,9 +3,26 @@
 <main id="content" class="site-content">
   <?php
   if (have_posts()) :
-    while (have_posts()) : the_post();
-      get_template_part('template-parts/content', 'page');
-    endwhile;
+    while (have_posts()) : the_post(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+          <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+        </header>
+
+        <div class="entry-content">
+          <?php
+          the_content();
+
+          wp_link_pages(array(
+            'before' => '<div class="page-links">' . __('Pages:', 'your-theme-textdomain'),
+            'after'  => '</div>',
+          ));
+          ?>
+        </div>
+      </article>
+
+    <?php endwhile;
   else :
     get_template_part('template-parts/content', 'none');
   endif;
