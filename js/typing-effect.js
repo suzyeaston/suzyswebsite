1d7f5554ce142201e2f2aadd56252f917e20e57d
Suzy Easton <suzyeaston@icloud.com>
2023-04-09 19:52:33 -0700

    Update typing-effect.js

--- js/typing-effect.js
+++ js/typing-effect.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
 From Vancouver, BC, during the early 80s, a new person came into existence. Suzy Easton, our protagonist, dedicated her early years to competitve swimming, with dreams of olympic triumph...  
 
 However, when she turned 14, Suzy realized that her competitive swimming days were numbered. 
-Hungry for a new challenge, she traded her swim cap for a guitar pick. 
+Ready for a new challenge, she traded her swim cap for a guitar pick. 
 Little did she know that this was only the beginning of an incredible journey that would take her across Canada, from music to IT, 
-  and eventually lead her back home to Metro Vancouver, in Burnaby.
+  and eventually lead her back home to Metro Vancouver, in Burnaby, recording music and making video games...
 
 Join Suzy as she navigates through the levels of life, building an extraordinary story of personal growth, adventure, and self-discovery. Will she find her ultimate quest? Press START to begin the adventure.";
   let currentIndex = 0;
