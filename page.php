eec46180876db9bc9bd48960c1e71041c92422fa
Suzy Easton <suzyeaston@icloud.com>
2023-04-09 17:00:13 -0700

    Committing local changes

--- page.php
+++ page.php
@@ -8,11 +8,11 @@
     while (have_posts()) : the_post(); ?>
 
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <header class="entry-header" id="level-title">
+        <header class="entry-header">
           <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
         </header>
 
-        <div class="entry-content" id="level-content">
+        <div class="entry-content">
           <?php the_content(); ?>
         </div>
       </article>
