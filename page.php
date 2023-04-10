050d3a65f682f8ed73cd9227ed2dd47230771a51
Suzy Easton <suzyeaston@icloud.com>
2023-04-09 19:31:22 -0700

    Create page.php

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+<main id="content" class="site-content">
+  <?php if (is_front_page()): ?>
+    <div id="my-name">Suzy Easton</div>
+    <div id="player-start">PLAYER 1</div>
+  <?php endif; ?>
+  <?php
+  if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+          <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+        </header>
+
+        <div class="entry-content">
+          <?php if (is_page('level-1')): ?>
+            <div id="bio-text"></div>
+          <?php endif; ?>
+          <?php the_content(); ?>
+        </div>
+      </article>
+
+    <?php endwhile;
+  else :
+    get_template_part('template-parts/content', 'none');
+  endif;
+  ?>
+</main>
+
+<?php get_footer(); ?>
