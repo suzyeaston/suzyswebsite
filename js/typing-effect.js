09f3cf68b34b9d4eb18cf656c30a2e62befb094d
Suzy Easton <suzyeaston@icloud.com>
2023-04-09 19:57:47 -0700

    i created a syntax error that caused the bio not to display

--- js/typing-effect.js
+++ js/typing-effect.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const bioText = document.getElementById("bio-text");
-  const bioContent =
-    "Welcome to Level 1: The Origin of Suzy's Epic Journey
-
-From Vancouver, BC, during the early 80s, a new person came into existence. Suzy Easton, our protagonist, dedicated her early years to competitve swimming, with dreams of olympic triumph...  
-
-However, when she turned 14, Suzy realized that her competitive swimming days were numbered. 
-Ready for a new challenge, she traded her swim cap for a guitar pick. 
-Little did she know that this was only the beginning of an incredible journey that would take her across Canada, from music to IT, 
-  and eventually lead her back home to Metro Vancouver, in Burnaby, recording music and making video games...
-
-Join Suzy as she navigates through the levels of life, building an extraordinary story of personal growth, adventure, and self-discovery. Will she find her ultimate quest? Press START to begin the adventure.";
+  const bioContent = `Welcome to Level 1: The Origin of Suzy's Epic Journey
+From Vancouver, BC, during the early 80s, a new person came into existence. Suzy Easton, our protagonist, dedicated her early years to competitive swimming, with dreams of Olympic triumph...
+However, when she turned 14, Suzy realized that her competitive swimming days were numbered.
+Ready for a new challenge, she traded her swim cap for a guitar pick.
+Little did she know that this was only the beginning of an incredible journey that would take her across Canada, from music to IT,
+and eventually lead her back home to Metro Vancouver, in Burnaby, recording music and making video games...
+Join Suzy as she navigates through the levels of life, building an extraordinary story of personal growth, adventure, and self-discovery. Will she find her ultimate quest? Press START to begin the adventure.`;
   let currentIndex = 0;
 
   function typeWriter() {
@@ -25,3 +21,9 @@ Join Suzy as she navigates through the levels of life, building an extraordinary
     typeWriter();
   }
 });
+
+
+
+
+
+
