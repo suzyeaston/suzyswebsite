43e2e63b8f70f1411fe6585e0361ee25e6bfb6d8
Suzy Easton <suzyeaston@icloud.com>
2023-04-11 01:00:46 -0700

    testing simulating an F1 car driving by and then animating my email address with a fade-in effect, will this work? lol

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const email = document.getElementById("contact-email");
+
+  if (!email) {
+    return;
+  }
+
+  function animateEmail() {
+    email.style.transition = "opacity 1s";
+    email.style.opacity = "1";
+  }
+
+  function simulateF1CarDrive() {
+    setTimeout(animateEmail, 1500);
+  }
+
+  simulateF1CarDrive();
+});
