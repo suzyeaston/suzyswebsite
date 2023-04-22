254680621b3fd4c85e68e14df569470e07dd6c30
Suzy Easton <suzyeaston@icloud.com>
2023-04-21 20:41:24 -0700

    okay maybe this will show the guitar hahahaha

--- js/typing-effect.js
+++ js/typing-effect.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const bioText = document.getElementById("bio-text");
+  const guitarCanvas = document.getElementById("guitar-canvas");
+  const startGameButton = document.getElementById("start-game-button");
   const bioContent = `In the year 2023, Suzy Easton, a single 40-year-old musician from Vancouver, finds herself battling cosmic foes! Once a national touring bassist, Canucks fan, taco lover, and poker enthusiast, Suzy now faces an 80s arcade-style adventure in a universe where harmony is under siege. Can she restore balance to the galaxy, 
   just as the Canucks defied the odds to reach their first-ever Stanley Cup Final in 1982 hahahahaha? Join Suzy's quest against the Galactic Groove Invaders!`;
 
