5dc64e06c6576b25963791a68b9cebd54ee0fc49
Suzy Easton <suzyeaston@icloud.com>
2023-04-21 20:22:51 -0700

    okay lets see if this trippy guitar works

--- js/typing-effect.js
+++ js/typing-effect.js
@@ -10,7 +10,75 @@ document.addEventListener("DOMContentLoaded", function () {
       bioText.innerHTML += bioContent.charAt(currentIndex);
       currentIndex++;
       setTimeout(typeWriter, 50);
+    } else {
+      showGuitarAndButton();
     }
+  }
+
+  function showGuitarAndButton() {
+    if (guitarCanvas && startGameButton) {
+      guitarCanvas.style.display = "block";
+      startGameButton.style.display = "block";
+      startGameButton.classList.add("green-theme");
+      drawGuitar(guitarCanvas);
+    }
+  }
+
+  function drawGuitar(canvas) {
+    const ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "#00ff00";
+    ctx.lineWidth = 4;
+
+    // Draw the head
+    ctx.beginPath();
+    ctx.moveTo(60, 40);
+    ctx.lineTo(240, 40);
+    ctx.lineTo(240, 180);
+    ctx.lineTo(60, 180);
+    ctx.lineTo(60, 40);
+    ctx.stroke();
+
+    // Draw the neck
+    ctx.beginPath();
+    ctx.moveTo(120, 180);
+    ctx.lineTo(180, 180);
+    ctx.lineTo(180, 600);
+    ctx.lineTo(120, 600);
+    ctx.lineTo(120, 180);
+    ctx.stroke();
+
+    // Draw the body
+    ctx.beginPath();
+    ctx.moveTo(60, 600);
+    ctx.lineTo(240, 600);
+    ctx.lineTo(280, 750);
+    ctx.lineTo(20, 750);
+    ctx.lineTo(60, 600);
+    ctx.stroke();
+
+    // Draw the strings
+    for (let i = 0; i < 4; i++) {
+      ctx.beginPath();
+      ctx.moveTo(120 + i * 20, 40);
+      ctx.lineTo(120 + i * 20, 750);
+      ctx.stroke();
+    }
+
+    // Draw the tuning pegs
+    for (let i = 0; i < 4; i++) {
+      ctx.beginPath();
+      ctx.arc(90 + i * 50, 60, 10, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+
+    // Draw the bridge
+    ctx.beginPath();
+    ctx.arc(60, 700, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(240, 700, 10, 0, 2 * Math.PI);
+    ctx.stroke();
   }
 
   if (bioText) {
