1a2792c45508b269927057c0fad478ac45ba51cc
Suzy Easton <suzyeaston@icloud.com>
2023-04-22 00:56:59 -0700

    please don't crash lol

--- functions.php
+++ functions.php
@@ -6,12 +6,14 @@ add_action('after_setup_theme', 'suzyeaston_theme_setup');
 
 function suzyeaston_scripts() {
   wp_enqueue_style('suzyswebsite-main-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-  wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), '1.0', 'all'); // Add this line
+  wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), '1.0', 'all');
+  
   wp_enqueue_script('suzyswebsite-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0', true);
   wp_enqueue_script('suzyswebsite-galaxy-js', get_template_directory_uri() . '/js/galaxy.js', array('jquery'), '1.0', true);
 
   if (is_page('contact')) {
-    wp_enqueue_script('suzyswebsite-contact-js', get_template_directory_uri() . '/js/contact.js', array('jquery'), '1.0', true);
+    wp_enqueue_style('suzyswebsite-retro-style', get_template_directory_uri() . '/retro-style.css', array(), '1.0', 'all');
+    wp_enqueue_script('suzyswebsite-email-animation-js', get_template_directory_uri() . '/js/email-animation.js', array(), '1.0', true);
   }
 
   wp_enqueue_script('suzyswebsite-typing-effect-js', get_template_directory_uri() . '/js/typing-effect.js', array(), '1.0', true);
@@ -28,4 +30,3 @@ function suzyeaston_menus() {
 }
 add_action('init', 'suzyeaston_menus');
 ?>
-
