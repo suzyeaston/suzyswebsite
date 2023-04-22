66934ad6b9e354b8695294b79a656ae767a162bb
Suzy Easton <suzyeaston@icloud.com>
2023-04-21 19:41:50 -0700

    enqueuing the "border-animation.css" file because yeah baby

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ add_action('after_setup_theme', 'suzyeaston_theme_setup');
 
 function suzyeaston_scripts() {
   wp_enqueue_style('suzyswebsite-main-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+  wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), '1.0', 'all'); // Add this line
   wp_enqueue_script('suzyswebsite-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0', true);
   wp_enqueue_script('suzyswebsite-galaxy-js', get_template_directory_uri() . '/js/galaxy.js', array('jquery'), '1.0', true);
 
@@ -27,3 +28,4 @@ function suzyeaston_menus() {
 }
 add_action('init', 'suzyeaston_menus');
 ?>
+
