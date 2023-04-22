a7544d43dc30849e5c4ff11e893d3fd91573f31d
Suzy Easton <suzyeaston@icloud.com>
2023-04-21 21:45:02 -0700

    okay we just want navigation to be consistent lol

--- header.php
+++ header.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+  <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-<div id="starry-background"></div>
-<header>
+  <?php wp_body_open(); ?>
+  <div id="starry-background"></div>
+  <header class="site-header">
     <nav>
-        <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+      <ul>
+        <li><a href="<?php echo esc_url(home_url('/')); ?>">Home</a></li>
+        <li><a href="<?php echo esc_url(home_url('/contact')); ?>">Contact</a></li>
+        <li><a href="<?php echo esc_url(home_url('/suzys-online-80s-video-game')); ?>">Suzy's Online 80s Video Game</a></li>
+      </ul>
     </nav>
-</header>
+  </header>
+
