248bc6702034ee255b6b224a41a59c1876e00c83
Suzy Easton <suzyeaston@icloud.com>
2023-04-22 18:11:59 -0700

    Burnaby, BC baby!

--- page-contact.php
+++ page-contact.php
@@ -10,7 +10,7 @@
       <div id="contact-details">
         <h1 class="how-to-reach-suzy">How to reach Suzy</h1>
         <div class="contact-item" id="contact-name">Suzy Easton</div>
-        <div class="contact-item" id="contact-location">Vancouver, BC</div>
+        <div class="contact-item" id="contact-location">Burnaby, BC</div>
         <div class="contact-item" id="contact-email">info@suzyeaston.tech</div>
         <div class="contact-item" id="contact-instagram"><a href="https://www.instagram.com/officialsuzyeaston/" target="_blank">officialsuzyeaston (Instagram)</a></div>
         <div class="contact-item" id="contact-twitter"><a href="https://twitter.com/officialsuzye" target="_blank">officialsuzye (Twitter)</a></div>
