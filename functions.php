fcc8fdb8c11aa770c595b98c18f961abd98f82f7
Suzy Easton <suzyeaston@icloud.com>
2023-04-23 18:42:25 -0700

    i am trying to prevent caching for other users because i update all the time hahaha

--- functions.php
+++ functions.php
@@ -5,19 +5,22 @@ function suzyeaston_theme_setup() {
 add_action('after_setup_theme', 'suzyeaston_theme_setup');
 
 function suzyeaston_scripts() {
-  wp_enqueue_style('suzyswebsite-main-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-  wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), '1.0', 'all');
-  
-  wp_enqueue_script('suzyswebsite-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0', true);
-  wp_enqueue_script('suzyswebsite-galaxy-js', get_template_directory_uri() . '/js/galaxy.js', array('jquery'), '1.0', true);
+  $theme_version = wp_get_theme()->get('Version');
+  $timestamp = filemtime(get_stylesheet_directory() . '/style.css');
+
+  wp_enqueue_style('suzyswebsite-main-style', get_template_directory_uri() . '/style.css', array(), $theme_version . '-' . $timestamp, 'all');
+  wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), $theme_version . '-' . $timestamp, 'all');
+
+  wp_enqueue_script('suzyswebsite-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), $theme_version . '-' . $timestamp, true);
+  wp_enqueue_script('suzyswebsite-galaxy-js', get_template_directory_uri() . '/js/galaxy.js', array('jquery'), $theme_version . '-' . $timestamp, true);
 
   if (is_page('contact')) {
-    wp_enqueue_style('suzyswebsite-retro-style', get_template_directory_uri() . '/retro-style.css', array(), '1.0', 'all');
-    wp_enqueue_script('suzyswebsite-email-animation-js', get_template_directory_uri() . '/js/email-animation.js', array(), '1.0', true);
+    wp_enqueue_style('suzyswebsite-retro-style', get_template_directory_uri() . '/retro-style.css', array(), $theme_version . '-' . $timestamp, 'all');
+    wp_enqueue_script('suzyswebsite-email-animation-js', get_template_directory_uri() . '/js/email-animation.js', array(), $theme_version . '-' . $timestamp, true);
   }
 
-  wp_enqueue_script('suzyswebsite-nav-listener-js', get_template_directory_uri() . '/js/nav-listener.js', array('jquery'), '1.0', true); // Add this line
-  wp_enqueue_script('suzyswebsite-typing-effect-js', get_template_directory_uri() . '/js/typing-effect.js', array(), '1.0', true);
+  wp_enqueue_script('suzyswebsite-nav-listener-js', get_template_directory_uri() . '/js/nav-listener.js', array('jquery'), $theme_version . '-' . $timestamp, true);
+  wp_enqueue_script('suzyswebsite-typing-effect-js', get_template_directory_uri() . '/js/typing-effect.js', array(), $theme_version . '-' . $timestamp, true);
 }
 add_action('wp_enqueue_scripts', 'suzyeaston_scripts', 5);
 
