943e54c343abca4a1e0c23d451ba9a412abc2bbc
Suzy Easton <suzyeaston@icloud.com>
2023-05-01 23:46:33 -0700

    adding burnaby skyline, dont crash

--- functions.php
+++ functions.php
@@ -9,6 +9,10 @@ function suzyeaston_scripts() {
   wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), '1.0', 'all');
   wp_enqueue_script('suzyswebsite-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0', true);
   wp_enqueue_script('suzyswebsite-galaxy-js', get_template_directory_uri() . '/js/galaxy.js', array('jquery'), '1.0', true);
+  wp_enqueue_script('suzyswebsite-skyline-js', get_template_directory_uri() . '/js/skyline.js', array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'suzyeaston_scripts', 5);
+
 
   if (is_page('contact')) {
     wp_enqueue_script('suzyswebsite-contact-js', get_template_directory_uri() . '/js/contact.js', array('jquery'), '1.0', true);
