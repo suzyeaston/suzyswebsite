87e7a3749656e58f0271e9707de6ace79fe4c475
Suzy Easton <suzyeaston@icloud.com>
2023-05-04 23:27:41 -0700

    Update galaxy.js -i'm trying to make it smart haha

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,103 +1,103 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
   const starryBackground = document.getElementById("starry-background");
-  const playerStart = document.getElementById("player-start");
-  const myName = document.getElementById("my-name");
 
-  let playerStartVisible = false;
+  if (starryBackground) {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    const playerStart = document.getElementById("player-start");
+    const myName = document.getElementById("my-name");
 
-  function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
+    let playerStartVisible = false;
 
-  function drawStars(layer, speed) {
-    const starCount = 100 * layer;
-
-    for (let i = 0; i < starCount; i++) {
-      const x = Math.random() * canvas.width;
-      const y = Math.random() * canvas.height;
-      const radius = Math.random() * layer;
-      const brightness = Math.random() * 0.5 + 0.3 - 0.1 * (layer - 1);
-
-      context.beginPath();
-      context.arc(x, y, radius, 0, 2 * Math.PI, false);
-      context.fillStyle = `rgba(255, 255, 255, ${brightness})`;
-      context.fill();
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
     }
-  }
 
-  function drawBackground() {
-    context.fillStyle = "rgba(0, 0, 0, 0.8)";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-  }
+    function drawStars(layer, speed) {
+      const starCount = 100 * layer;
 
-  function createStarryBackground() {
-    resizeCanvas();
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawBackground();
+      for (let i = 0; i < starCount; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        const radius = Math.random() * layer;
+        const brightness = Math.random() * 0.5 + 0.3 - 0.1 * (layer - 1);
 
-    for (let layer = 1; layer <= 3; layer++) {
-      drawStars(layer, layer);
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = `rgba(255, 255, 255, ${brightness})`;
+        context.fill();
+      }
     }
-  }
 
-  starryBackground.appendChild(canvas);
-  createStarryBackground();
+    function drawBackground() {
+      context.fillStyle = "rgba(0, 0, 0, 0.8)";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
 
-  window.addEventListener("resize", () => {
-    resizeCanvas();
+    function createStarryBackground() {
+      resizeCanvas();
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      drawBackground();
+
+      for (let layer = 1; layer <= 3; layer++) {
+        drawStars(layer, layer);
+      }
+    }
+
+    starryBackground.appendChild(canvas);
     createStarryBackground();
-  });
 
-  window.addEventListener("scroll", () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawBackground();
+    window.addEventListener("resize", () => {
+      resizeCanvas();
+      createStarryBackground();
+    });
 
-    for (let layer = 1; layer <= 3; layer++) {
-      context.save();
-      context.translate(0, window.pageYOffset * (1 - layer / 5));
-      drawStars(layer, layer);
-      context.restore();
-    }
-  });
+    window.addEventListener("scroll", () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      drawBackground();
 
-  playerStart.addEventListener("click", () => {
-    if (!playerStartVisible) {
-      playerStart.style.display = "none";
-      myName.style.filter = "none";
-      playerStartVisible = true;
+      for (let layer = 1; layer <= 3; layer++) {
+        context.save();
+        context.translate(0, window.pageYOffset * (1 - layer / 5));
+        drawStars(layer, layer);
+        context.restore();
+      }
+    });
 
-      // Create Player 1 Ready? overlay and buttons
-      const playerReadyOverlay = document.createElement("div");
-      playerReadyOverlay.id = "player-ready-overlay";
+    playerStart.addEventListener("click", () => {
+      if (!playerStartVisible) {
+        playerStart.style.display = "none";
+        myName.style.filter = "none";
+        playerStartVisible = true;
 
-      const playerReadyText = document.createElement("div");
-      playerReadyText.id = "player-ready-text";
-      playerReadyText.innerText = "Player 1 Ready?";
+        // Create Player 1 Ready? overlay and buttons
+        const playerReadyOverlay = document.createElement("div");
+        playerReadyOverlay.id = "player-ready-overlay";
 
-      const playerReadyYes = document.createElement("button");
-      playerReadyYes.id = "player-ready-yes";
-      playerReadyYes.innerText = "Yes";
-      playerReadyYes.addEventListener("click", () => {
-        window.location.href = "https://suzyeaston.tech/suzys-online-80s-video-game/";
-      });
+        const playerReadyText = document.createElement("div");
+        playerReadyText.id = "player-ready-text";
+        playerReadyText.innerText = "Player 1 Ready?";
 
-      const playerReadyNo = document.createElement("button");
-      playerReadyNo.id = "player-ready-no";
-      playerReadyNo.innerText = "No";
-      playerReadyNo.addEventListener("click", () => {
-        playerReadyOverlay.remove();
-        playerStart.style.display = "block";
-        playerStartVisible = false;
-      });
+        const playerReadyYes = document.createElement("button");
+        playerReadyYes.id = "player-ready-yes";
+        playerReadyYes.innerText = "Yes";
+        playerReadyYes.addEventListener("click", () => {
+          window.location.href = "https://suzyeaston.tech/suzys-online-80s-video-game/";
+        });
 
-      playerReadyOverlay.appendChild(playerReadyText);
-      playerReadyOverlay.appendChild(playerReadyYes);
-      playerReadyOverlay.appendChild(playerReadyNo);
+        const playerReadyNo = document.createElement("button");
+        playerReadyNo.id = "player-ready-no";
+        playerReadyNo.innerText = "No";
+        playerReadyNo.addEventListener("click", () => {
+          playerReadyOverlay.remove();
+          playerStart.style.display = "block";
+          playerStartVisible = false;
+        });
 
-      document.body.appendChild(playerReadyOverlay);
-    }
-  });
+        playerReadyOverlay.appendChild(playerReadyText);
+        playerReadyOverlay.appendChild(playerReadyYes);
+        playerReadyOverlay.appendChild(playerReadyNo);
+
+        document.body.appendChild(playerReadyOverlay);});
 });
