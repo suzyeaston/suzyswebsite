d4366d2b1dfc9c8ce8754b2581db0604c8a785d2
Suzy Easton <suzyeaston@icloud.com>
2023-05-05 00:50:22 -0700

    Update functions.php - hrm

--- functions.php
+++ functions.php
@@ -3,13 +3,13 @@ function suzyeaston_theme_setup() {
   add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'suzyeaston_theme_setup');
+
 function suzyeaston_scripts() {
   wp_enqueue_style('suzyswebsite-main-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
   wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), '1.0', 'all');
   wp_enqueue_script('suzyswebsite-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0', true);
   wp_enqueue_script('suzyswebsite-galaxy-js', get_template_directory_uri() . '/js/galaxy.js', array('jquery'), '1.0', true);
   wp_enqueue_script('suzyswebsite-skyline-js', get_template_directory_uri() . '/js/skyline.js', array('jquery'), '1.0', true);
-  }
 
   wp_enqueue_script('suzyswebsite-typing-effect-js', get_template_directory_uri() . '/js/typing-effect.js', array(), '1.0', true);
 }
