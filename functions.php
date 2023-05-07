6c5aefd27afbb7e30b4771fb444438130d9f3829
Suzy Easton <suzyeaston@icloud.com>
2023-05-07 13:29:06 -0700

    Update functions.php - scoreboard.js script will only be enqueued on the front page

--- functions.php
+++ functions.php
@@ -8,11 +8,13 @@ function suzyeaston_scripts() {
   wp_enqueue_style('suzyswebsite-main-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
   wp_enqueue_style('suzyswebsite-border-animation', get_template_directory_uri() . '/border-animation.css', array(), '1.0', 'all');
   wp_enqueue_script('suzyswebsite-custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0', true);
-  wp_enqueue_script('suzyswebsite-scoreboard-js', get_template_directory_uri() . '/js/scoreboard.js', array(), '1.0', true);
   wp_enqueue_script('suzyswebsite-galaxy-js', get_template_directory_uri() . '/js/galaxy.js', array('jquery'), '1.0', true);
   wp_enqueue_script('suzyswebsite-skyline-js', get_template_directory_uri() . '/js/skyline.js', array('jquery'), '1.0', true);
-
   wp_enqueue_script('suzyswebsite-typing-effect-js', get_template_directory_uri() . '/js/typing-effect.js', array(), '1.0', true);
+
+  if (is_front_page()) {
+    wp_enqueue_script('suzyswebsite-scoreboard-js', get_template_directory_uri() . '/js/scoreboard.js', array(), '1.0', true);
+  }
 }
 add_action('wp_enqueue_scripts', 'suzyswebsite_scripts');
 
