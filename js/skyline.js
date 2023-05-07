d8ca8818528ef72b9042f337e8da72f378feb2e4
Suzy Easton <suzyeaston@icloud.com>
2023-05-07 16:17:05 -0700

    Update skyline.js

--- js/skyline.js
+++ js/skyline.js
@@ -4,14 +4,21 @@ function drawSkyline(context, skyline) {
   const width = context.canvas.width / skyline.length;
   const height = context.canvas.height;
 
-  let gradient = context.createLinearGradient(0, 0, 0, height);
-  gradient.addColorStop(0, '#00FF00');
-  gradient.addColorStop(1, '#000');
-  context.fillStyle = gradient;
-
   for (let i = 0; i < skyline.length; i++) {
     const buildingHeight = skyline[i] / 10 * height;
+    context.fillStyle = `rgba(50, 205, 50, ${Math.random() * 0.8 + 0.2})`; // Change building color and opacity
     context.fillRect(i * width, height - buildingHeight, width, buildingHeight);
+
+    // Add window lights
+    context.fillStyle = `rgba(255, 255, 255, ${Math.random() * 0.8 + 0.2})`; // Change window light color and opacity
+    const windowSize = 4;
+    for (let j = 0; j < buildingHeight; j += windowSize * 2) {
+      for (let k = 0; k < width; k += windowSize * 2) {
+        if (Math.random() < 0.5) {
+          context.fillRect(i * width + k, height - buildingHeight + j, windowSize, windowSize);
+        }
+      }
+    }
   }
 }
 
