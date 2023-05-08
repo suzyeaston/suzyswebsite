f9541fa9ab4136015f365a0fb9757e5a8779682f
Suzy Easton <suzyeaston@icloud.com>
2023-05-07 20:33:36 -0700

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <main id="content" class="site-content">
+  <div id="starry-background"></div>
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
       <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
@@ -11,7 +12,11 @@
         <h1 class="how-to-reach-suzy">How to reach Suzy</h1>
         <div class="contact-item" id="contact-name">Suzy Easton</div>
         <div class="contact-item" id="contact-location">Burnaby, BC</div>
-        <div class="contact-item" id="contact-email">info@suzyeaston.tech</div>
+        <div class="contact-item" id="contact-email">
+          <script>
+            document.write('<a href="mailto:' + 'info' + '@' + 'suzyeaston' + '.' + 'tech">info@suzyeaston.tech</a>');
+          </script>
+        </div>
         <div class="contact-item" id="contact-instagram"><a href="https://www.instagram.com/officialsuzyeaston/" target="_blank">officialsuzyeaston (Instagram)</a></div>
         <div class="contact-item" id="contact-twitter"><a href="https://twitter.com/officialsuzye" target="_blank">officialsuzye (Twitter)</a></div>
       </div>
