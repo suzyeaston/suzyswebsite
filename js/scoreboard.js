35f4b83ed15297532aa86d13f9e3b64d9d57ef07
Suzy Easton <suzyeaston@icloud.com>
2023-05-13 17:56:36 -0700

    Update scoreboard.js - yeaaaaa including baseball

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,23 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-  getResults();
-});
-
-async function getResults() {
-  const leagues = [
-    { name: "NHL", endpoint: "latesticehockey.php" },
-    { name: "NBA", endpoint: "latestbasketball.php" },
-    { name: "Horse Racing", endpoint: "latestracing.php" },
-    { name: "MLB", endpoint: "latestbaseball.php" }
-  ];
-
-  for (const league of leagues) {
-    const data = await fetchResults(league.endpoint);
-    displayResults(league.name, data.results);
-  }
-}
-
-async function fetchResults(endpoint) {
-  const response = await fetch(`https://www.thesportsdb.com/api/v1/json/60130162/${endpoint}`);
+async function fetchResults(leagueId) {
+  const response = await fetch(`https://www.thesportsdb.com/api/v1/json/60130162/eventresults.php?id=${leagueId}`);
   const data = await response.json();
   return data;
 }
@@ -32,18 +14,45 @@ function displayResults(leagueName, results) {
 
     const resultList = document.createElement("ul");
 
-    if (results && results.length > 0) { // Check if results is not null and has items
+    if (results && results.length > 0) {
       results.forEach(result => {
         const listItem = document.createElement("li");
         listItem.textContent = `${result.strEvent}: ${result.intHomeScore}-${result.intAwayScore}`;
+        listItem.style.fontFamily = "'Press Start 2P', cursive";
+        listItem.style.fontSize = "12px";
+        listItem.style.color = "#00FF00";
+        listItem.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        listItem.style.padding = "5px";
+        listItem.style.borderRadius = "5px";
+        listItem.style.marginBottom = "5px";
         resultList.appendChild(listItem);
       });
     } else {
       const listItem = document.createElement("li");
       listItem.textContent = "No results available";
+      listItem.style.fontFamily = "'Press Start 2P', cursive";
+      listItem.style.fontSize = "12px";
+      listItem.style.color = "#00FF00";
+      listItem.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+      listItem.style.padding = "5px";
+      listItem.style.borderRadius = "5px";
+      listItem.style.marginBottom = "5px";
       resultList.appendChild(listItem);
     }
 
     container.appendChild(resultList);
   }
 }
+
+const leagues = [
+  { name: "NHL", id: "4380" },
+  { name: "NBA", id: "4387" },
+  { name: "MLB", id: "4424" }
+];
+
+window.onload = () => {
+  leagues.forEach(async (league) => {
+    const results = await fetchResults(league.id);
+    displayResults(league.name, results);
+  });
+};
