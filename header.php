3e7d786232be78f48a00df5ca7d06ad9d69b0023
Suzy Easton <suzyeaston@icloud.com>
2023-05-13 18:11:18 -0700

    Update header.php - trying to reduce caching

--- header.php
+++ header.php
@@ -5,8 +5,16 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Pragma" content="no-cache" />
+  <meta http-equiv="Expires" content="0" />
   <?php wp_head(); ?>
 </head>
+  
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+  <div id="starry-background"></div>
+  <header class="site-header">
 
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
